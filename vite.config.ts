f28bea5830147488f31de5b8cce7c460f62e5e04
Yusen <1204367216@qq.com>
2024-06-21 17:08:26 +0800

    更改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,47 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
   plugins: [
     vue(),
-    VueSetupExtend(),// vue-plugin-vue-setup-extend 新增的插件
   ],
+  // 静态资源基础路径 base: './' || '',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // 配置目录别名
+      '@': resolve(__dirname, 'src'),
+      'views': resolve(__dirname, 'src/views'),
+      'utils': resolve(__dirname, 'src/utils'),
+    },
+  },
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // 全局less变量存储路径(配置less的全局变量)
+          hack: `true; @import (reference) "${resolve('src/public/config.less')}";`,
+        },
+        javascriptEnabled: true,
+      }
     }
-  }
+  },
+  // 开发服务器配置
+  server: {
+    host: true,
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: '//www.xxxx.com',
+        changeOrigin: true,
+        ws: true,
+        secure: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
 })
