4aa78f06ec643d7318f1d634cd3dced5c0538abb
Yusen <1204367216@qq.com>
2024-08-12 14:27:08 +0800

    2024-8-12

--- src/views/echartsChart/basicLineChart.vue
+++ src/views/echartsChart/basicLineChart.vue
@@ -9,8 +9,12 @@
 // 引入echarts
 import * as echarts from 'echarts';
 // 引入
-import { onMounted, nextTick, reactive } from 'vue';
-
+import { onMounted, ref, nextTick, reactive } from 'vue';
+// 定义 TimeDataItem 接口
+type typeDataList = {
+  timer: string;
+  price: number;
+}[];
 // 一进来就调用
 onMounted(() => {
   drawChart(newPrArr, newTimArr, endSameArr);
@@ -27,17 +31,17 @@ const dataList = reactive([
   { timer: '2024-07-20 14:36:52', price: 559.0 }, // 写三个相同的数据
   { timer: '2024-08-08 10:56:12', price: 569.0 },
   { timer: '2024-08-10 15:10:02', price: 489.0 },
-]);
+]) as typeDataList;
 
 // 设置空数组存放重置好的原数据
-let newLineArr = reactive([]);
+let newLineArr = ref<string[]>([]);
 // 遍历原数据把定好的数据存放到 newLineArr 中
 dataList.forEach((item) => {
   let date = new Date(item.timer).getTime() / 1000 + ':' + item.price * 100;
-  newLineArr.push(date);
+  newLineArr.value.push(date);
 });
-// console.log(newLineArr);
-/* 
+// console.log(newLineArr.value);
+/*
 打印如下：转换时间戳和价格的格式通过 ： 分隔开
 0: "1723171353:51490"
 1: "1723168142:49590"
@@ -51,8 +55,8 @@ dataList.forEach((item) => {
 */
 
 // JS统计重复元素
-function addSameElement(arr) {
-  let count = {};
+function addSameElement<T extends string | number>(arr: T[]): Record<T, number> {
+  let count: Record<T, number> = {} as Record<T, number>;
   for (let i = 0; i < arr.length; i++) {
     if (count[arr[i]]) {
       count[arr[i]]++;
@@ -64,10 +68,10 @@ function addSameElement(arr) {
 }
 
 // 统计重置好的原数据中有多少个相同的数据
-const sameElement = addSameElement(newLineArr);
+const sameElement = addSameElement(newLineArr.value);
 // console.log(sameElement);
 
-/* 
+/*
 打印如下所示：后面的数字代表有多少个相同的
 1721457412:55900: 3
 1723085772:56900: 1
@@ -78,7 +82,7 @@ const sameElement = addSameElement(newLineArr);
 */
 
 // 时间戳转日期格式
-function timestampToDate(timestamp) {
+function timestampToDate(timestamp: any) {
   const date = new Date(timestamp);
   // 格式化日期和时间
   const year = date.getFullYear();
@@ -92,22 +96,22 @@ function timestampToDate(timestamp) {
 }
 
 // 创造新数组存放最终相同的数据元素
-const endSameArr = reactive([]);
+const endSameArr = reactive<any>([]);
 // 创造新数组存放最终的时间数据
-const newTimArr = reactive([]);
+const newTimArr = reactive<any>([]);
 // 创造新数组存放最终的价格数据
-const newPrArr = reactive([]);
+const newPrArr = reactive<any>([]);
 
 // 循环遍历 sameElement，取出需要的数据并存放到 newTimArr, newPrArr, endSameArr中
 for (let key in sameElement) {
   // 通过 ： 分隔开
   let splitArr = key.split(':');
   // 通过时间戳再次转换成日期格式并使用 slice() 方法去掉年
-  let newTime = timestampToDate(splitArr[0] * 1000).slice(5);
+  let newTime = timestampToDate((splitArr[0] as any) * 1000).slice(5);
   // 转换价格
-  let newPrice = splitArr[1] / 100;
+  let newPrice = (splitArr[1] as any) / 100;
   // 这里是后期 echarts 中 markPoint 所用到的数据格式
-  let rose = { name: key, value: sameElement[key], xAxis: newTime, yAxis: splitArr[1] / 100 };
+  let rose = { name: key, value: sameElement[key], xAxis: newTime, yAxis: (splitArr[1] as any) / 100 };
   if (sameElement[key] > 1) {
     // 如果 相同数据大于 1 的说明重复，需要利用起来再存到最终使用的新数组中
     endSameArr.push(rose);
@@ -117,7 +121,7 @@ for (let key in sameElement) {
   newPrArr.push(newPrice);
 }
 // console.log(endSameArr);
-/* 
+/*
 [
     0: {
       name: "1723168142:49590"
@@ -135,7 +139,7 @@ for (let key in sameElement) {
 */
 
 // console.log(newTimArr);
-/* 
+/*
 [
     0: "08-09 10:42:33"
     1: "08-09 09:49:02"
@@ -146,7 +150,7 @@ for (let key in sameElement) {
 ]
 */
 // console.log(newPrArr);
-/* 
+/*
 [
     0: 514.9
     1: 495.9
@@ -157,7 +161,7 @@ for (let key in sameElement) {
 ]
 */
 
-function drawChart(newPrArr, newTimArr, endSameArr) {
+function drawChart(newPrArr: any, newTimArr: any, endSameArr: any) {
   var myChart = echarts.init(document.getElementById('basicLine'));
   // 指定图表的配置项和数据
   // 该处就是图表内容，在官网的示例里面，要复制过来到项目里面的也是这一块内容
